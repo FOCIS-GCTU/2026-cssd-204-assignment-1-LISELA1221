05379f106491b947092b2ef9ae752097b25c1299
LISELA1221 <2425400863@live.gctu.edu.gh>
2026-06-01 05:08:37 +0800

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,38 @@
+#File:initials.py
+#Description:Print out my initials LSL in stylized large letters
+#Assignment Number:1
+#
+#Name:Lilian Sena Larbi
+#STUDENT ID:2425400863
+#EMAIL:2425400863@live.gctu.edu.gh
+#Grader:Lilian
+#
+#On my honor,Lilian Sena Larbi,this programming assignment is my own work
+#and I have not provided this code to any other student.
+from gc import is_finalized
+
 
 def main():
-  pass #code goes here
+
+    #This function prints initials LSL in large block letters
+    print()
+    print("...LSL")
+    print()
+    print("...LL.................SSSSSSSSSS..........LL................")
+    print("...LL................SSSSSSSSSSS..........LL.................")
+    print("...LL...............SSSS..................LL................")
+    print("...LL...............SSSS..................LL ...............")
+    print("...LL................SSSSS................LL................")
+    print("...LL...................SSSSSS............LL................")
+    print("...LL.......................SSSS..........LL................")
+    print("...LL.......................SSSS..........LL................")
+    print("...LL.............**...SSSSSSSSSSS...**...LL..............** ")
+    print("...LLLLLLLLLLLL...**...SSSSSSSSS.....**...LLLLLLLLLLLL....**")
+    print()
 
 
+#Two blank lines after the main
 
-main()
+
+if __name__=="__main__":
+    main()
